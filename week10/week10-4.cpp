08ddba852aa612b7bb6f4cca4b1f441595f686c7
yuhan4688 <12750131@gmail.com>
2024-11-14 15:18:36 +0800

    week10

---
+++ week10/week10-4.cpp
@@ -0,0 +1,13 @@
+//week10-4.cpp F020
+#include <iostream>
+using namespace std;
+int main()
+{
+	double h,w;
+	cin >> h >> w;
+	double bmi =w/(h*h);
+	cout << bmi << "\n";
+	if(bmi<18.5) cout << "too thin";
+	else if(bmi<24) cout << "standard";
+	else cout << "too fat";
+} 
