e5ea06fa1280235de0fbe56ec5f2ebd0940bf7a3
yuahn4688 <12750131@gmail.com>
2024-11-21 14:32:54 +0800

    week11

---
+++ week11/week11-1a.cpp
@@ -0,0 +1,16 @@
+//week11-1.cpp UVa11332 step01
+#include <iostream>
+using namespace std;
+int main()
+
+{
+	int n;
+	while(cin >> n){
+		if(n==0) break;
+		
+		
+		
+		cout << n << endl;
+	}
+}
+ 
---
+++ week11/week11-1b.cpp
@@ -0,0 +1,23 @@
+//week11-1.cpp UVa11332 step02 bo pi fa
+#include <iostream>
+using namespace std;
+int f(int n){
+	int ans=0;
+	while(n>0){
+		ans+=n%10;
+		n=n/10;
+	}
+	return ans;
+}
+int main()
+
+{
+	int n;
+	while(cin >> n){
+		if(n==0) break;
+		
+		
+		n=f(n);
+		cout << n << endl;
+	}
+}
